7ca00063bc261165edf332886eb037698b85ef8c
ItsNotAUsername <nik-off.fedotoff@yandex.ru>
2021-09-22 15:43:25 +0300

    some useful typeclasses

---
+++ src/main/scala/com/github/enki/util/typeclass/GenUUID.scala
@@ -0,0 +1,18 @@
+package com.github.enki
+package util
+package typeclass
+
+import cats.effect.Sync
+import java.util.UUID
+
+trait GenUUID[F[_]]:
+  def uuid: F[UUID]
+
+object GenUUID:
+
+  def apply[F[_]](using ev: GenUUID[F]): GenUUID[F] = ev
+
+  given genUuid4Sync[F[_]](using F: Sync[F]): GenUUID[F] with
+    def uuid: F[UUID] = F.delay(UUID.randomUUID())
+
+end GenUUID
---
+++ src/main/scala/com/github/enki/util/typeclass/Time.scala
@@ -0,0 +1,25 @@
+package com.github.enki
+package util
+package typeclass
+
+import cats.effect.Clock
+import java.time.{LocalDate, LocalDateTime, ZoneOffset}
+
+trait Time[F[_]]:
+  def localDate: F[LocalDate]
+  def localDateTime: F[LocalDateTime]
+
+object Time:
+
+  def apply[F[_]](using ev: Time[F]): Time[F] = ev
+
+  given fromClock[F[_]](using C: Clock[F]): Time[F] with
+    def localDate: F[LocalDate] = 
+      C.applicative
+        .map(C.realTimeInstant)(LocalDate.ofInstant(_, ZoneOffset.UTC))
+    
+    def localDateTime: F[LocalDateTime] = 
+      C.applicative
+        .map(C.realTimeInstant)(LocalDateTime.ofInstant(_, ZoneOffset.UTC))
+
+end Time
