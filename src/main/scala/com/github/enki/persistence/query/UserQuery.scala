ce3e01edfa5af949316205cf9602b95d4394c102
ItsNotAUsername <nik-off.fedotoff@yandex.ru>
2021-10-11 22:37:54 +0300

    Updated UserQuery

--- src/main/scala/com/github/enki/persistence/query/UserQuery.scala
+++ src/main/scala/com/github/enki/persistence/query/UserQuery.scala
@@ -5,55 +5,62 @@ package query
 import domain.{Email, Id}
 import domain.user.User
 import meta.given
+import model.UserRow
 
-import doobie.*
+import doobie.{Query0, Update0}
 import doobie.implicits.*
 import doobie.postgres.implicits.*
-import doobie.util.fragments.whereAnd
 import java.util.UUID
 
 private[persistence] object UserQuery:
 
   private val selectFragment = 
     fr"""
-      SELECT u.id, u.username, u.email, u.password, u.active, u.code, u.created, u.updated
+      SELECT u.id, 
+             u.username, 
+             u.email, 
+             u.password, 
+             u.active, 
+             u.code, 
+             u.created, 
+             u.updated
         FROM usr u
     """
 
-  def selectUserById(id: Id[User]): Query0[User] =
-    (selectFragment ++ whereAnd(fr"u.id = $id"))
-      .query[User]
+  def selectById(id: Id[User]): Query0[UserRow] =
+    (selectFragment ++ fr"WHERE u.id = $id")
+      .query[UserRow]
 
-  def selectUserByEmail(email: Email): Query0[User] =
-    (selectFragment ++ whereAnd(fr"u.email = $email"))
-      .query[User]
+  def selectByEmail(email: Email): Query0[UserRow] =
+    (selectFragment ++ fr"WHERE u.email = $email")
+      .query[UserRow]
 
-  def selectUserByCode(code: UUID): Query0[User] =
-    (selectFragment ++ whereAnd(fr"u.code = $code"))
-      .query[User]
+  def selectByCode(code: UUID): Query0[UserRow] =
+    (selectFragment ++ fr"WHERE u.code = $code")
+      .query[UserRow]
 
-  def insertUser(user: User): Update0 =
+  def insert(userRow: UserRow): Update0 =
     sql"""
       INSERT INTO usr (username, email, password, active, code, created, updated)
-           VALUES (${user.username},
-                   ${user.email},
-                   ${user.password},
-                   ${user.active},
-                   ${user.code},
-                   ${user.created},
-                   ${user.updated})
+           VALUES (${userRow.username},
+                   ${userRow.email},
+                   ${userRow.password},
+                   ${userRow.active},
+                   ${userRow.code},
+                   ${userRow.created},
+                   ${userRow.updated})
     """.update
 
-  def updateUser(user: User): Update0 =
+  def update(userRow: UserRow): Update0 =
     sql"""
       UPDATE usr
-         SET username = ${user.username},
-             email    = ${user.email},
-             password = ${user.password},
-             active   = ${user.active},
-             code     = ${user.code},
-             updated  = ${user.updated}
-       WHERE id = ${user.id}
+         SET username = ${userRow.username},
+             email    = ${userRow.email},
+             password = ${userRow.password},
+             active   = ${userRow.active},
+             code     = ${userRow.code},
+             updated  = ${userRow.updated}
+       WHERE id       = ${userRow.id}
     """.update
 
 end UserQuery
