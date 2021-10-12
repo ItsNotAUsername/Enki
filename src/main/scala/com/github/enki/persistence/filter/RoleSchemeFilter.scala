b85db80904db4404fdf5a8bff1fe6ca34bd4c845
ItsNotAUsername <nik-off.fedotoff@yandex.ru>
2021-10-12 21:37:04 +0300

    Filter for RoleScheme

---
+++ src/main/scala/com/github/enki/persistence/filter/RoleSchemeFilter.scala
@@ -0,0 +1,20 @@
+package com.github.enki
+package persistence
+package filter
+
+import domain.{Id, RoleSchemeName}
+import domain.workspace.Workspace
+import model.meta.given
+
+import doobie.Fragment
+import doobie.implicits.*
+import doobie.util.fragments.whereAndOpt
+
+final case class RoleSchemeFilter(
+  name:      Option[RoleSchemeName],
+  workspace: Option[Id[Workspace]]
+) extends Filter:
+  private val nameFragment      = name.map(n => fr"rs.name ILIKE '%$n%'")
+  private val workspaceFragment = workspace.map(id => fr"rs.workspace_id = $id")
+  
+  def fragment: Fragment = whereAndOpt(nameFragment, workspaceFragment)
